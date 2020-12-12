86086be0f86c91b5b2bc16682e518e7ec2c5f253
CortesJoan <73123760+CortesJoan@users.noreply.github.com>
2020-12-12 14:36:37 +0100

    Added Main Menu
    Added Main Menu folders and some assets

---
+++ Artistception/Assets/Scripts/MainMenuBehaviour.cs
@@ -0,0 +1,119 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuBehaviour : MonoBehaviour {
+	public GameObject optionsMenu;
+	SoundManager soundManager;
+
+
+	public void LoadLevel(string levelName){
+		SceneManager.LoadScene (levelName);
+	}
+ 
+ 
+	public void EndGame(){
+
+		#if UNITY_EDITOR 
+			UnityEditor.EditorApplication.isPlaying = false;
+		#else 
+			Application.Quit();
+		#endif
+	}
+	 
+	public void OpenOptions()
+	{
+	 
+		optionsMenu.SetActive(true);
+	}public void CloseOptions()
+	{
+	 
+		optionsMenu.SetActive(false);
+	}
+	private void UpdateQualityLabel()
+	{
+		int currentQuality = QualitySettings.GetQualityLevel();
+		string qualityName = QualitySettings.names[currentQuality];
+
+		optionsMenu.transform.Find("QualityLevel").GetComponent<UnityEngine.UI.Text>().text = "Quality: " + qualityName;
+
+	}
+
+	private void UpdateMasterVolumeLabel()
+	{
+		Debug.Log(AudioListener.volume);
+		float MasterAudioVolume = AudioListener.volume * 100;
+		optionsMenu.transform.Find("MasterVolume").GetComponent<UnityEngine.UI.Text>().text = "Master Volume: " + MasterAudioVolume.ToString("f2") + "%";
+		 
+	}private void UpdateBGMVolumeLabel()
+	{
+		 
+		float BgmAudioVolume = soundManager.AS[0].volume * 100;
+		 	optionsMenu.transform.Find("BGMVolume").GetComponent<UnityEngine.UI.Text>().text = "BGM Volume: " + BgmAudioVolume.ToString("f2") + "%";
+ }private void UpdateBGSVolumeLabel()
+	{
+		Debug.Log(AudioListener.volume);
+	 
+		float BgsAudioVolume = soundManager.AS[1].volume * 100;
+	 
+
+	 	optionsMenu.transform.Find("BGSVolume").GetComponent<UnityEngine.UI.Text>().text = "BGS Volume: " + BgsAudioVolume.ToString("f2") + "%";
+	 }private void UpdateSEVolumeLabel()
+	{ 
+		float SEAudioVolume = soundManager.AS[2].volume * 100;
+
+	 
+		optionsMenu.transform.Find("SEVolume").GetComponent<UnityEngine.UI.Text>().text = "SE Volume: " + SEAudioVolume.ToString("f2") + "%";
+	}
+
+	 
+
+	public void RestartGame()
+	{
+		SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+	}
+
+
+	public void IncreaseQuality()
+	{
+		QualitySettings.IncreaseLevel();
+		UpdateQualityLabel();
+	}
+
+	public void DecreaseQuality()
+	{
+		QualitySettings.DecreaseLevel();
+		UpdateQualityLabel();
+	}
+
+	public void SetMasterVolume(float value)
+	{
+		AudioListener.volume = value;
+		UpdateMasterVolumeLabel();
+	}public void SetBGMVolume(float value)
+	{
+		soundManager.AS[0].volume = value;
+		UpdateBGMVolumeLabel();
+	}public void SetBGSVolume(float value)
+	{
+		soundManager.AS[1].volume = value;
+		UpdateBGSVolumeLabel();
+	}public void SetSEVolume(float value)
+	{
+		soundManager.AS[2].volume = value;
+		UpdateSEVolumeLabel();
+	}
+
+	private void Start()
+	{
+		soundManager = FindObjectOfType<SoundManager>();
+		//Destroy(GameObject.Find("Main Camera").gameObject);
+		UpdateBGMVolumeLabel();
+		UpdateBGSVolumeLabel();
+		UpdateMasterVolumeLabel();
+		UpdateQualityLabel();
+		UpdateSEVolumeLabel();
+
+	}
+}
---
+++ Artistception/Assets/Scripts/SoundManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    [SerializeField]  public AudioSource[] AS;  // 0 bgm 1 bgs 2 se
+    // Start is called before the first frame update
+    void Start()
+    { 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void PlayBGM(AudioClip _clip)
+    {
+        AS[0].clip = _clip;
+        AS[0].Play();
+
+
+    }  public void PlayBGS(AudioClip _clip,bool _loop)
+    {
+        AS[1].clip = _clip;
+        AS[1].loop = _loop;
+        AS[1].Play();
+
+
+    }  public void PlaySE(AudioClip _clip, float _pitch)
+    {
+        ChangePitch(2, _pitch);
+        AS[2].PlayOneShot(_clip,5f);
+
+
+    }public void PlaySE(AudioClip _clip, float _pitch, float volume)
+    {
+        ChangePitch(2, _pitch);
+        AS[2].PlayOneShot(_clip,volume);
+
+
+    }
+    public void ChangePitch(int num, float _pitch) {
+        AS[num].pitch = _pitch;
+    }
+   
+}
