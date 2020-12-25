569bdcc7993fe2290c58ec0efbda2f9c60e395d2
CortesJoan <73123760+CortesJoan@users.noreply.github.com>
2020-12-25 11:36:32 +0100

    Added Player Jump
    Added player jump and fixed movement bugs

--- Artistception/Assets/Scripts/PlayerController.cs
+++ Artistception/Assets/Scripts/PlayerController.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -9,51 +10,144 @@ public class PlayerController : MonoBehaviour
 
     public const string RIGHT = "right";
     public const string LEFT = "left";
-
+    public const string JUMP = "space";
+    public KeyCode jump;
     string buttonPressed;
-
-
-    // Start is called before the first frame update
+    private float acceleration = 9.8f;
+    public bool isJumping = false;
+    private bool canJump = false;
+    public LayerMask groundMask;
+    public int DeploymentHeight = 3;
+    public List<String> inputList;
+    public float jumpForce;
+  
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
-        
-    }
 
-    //Put non physics based movement in here
+    }
+    // Los inputs se manejan en el Update
     void Update()
     {
-        Move();
+        InputHandler();
 
     }
+    bool pressedJumping;
 
-    //Put physica based movement in here
+
+     // Y en el FixedUpdate se hace el movimiento
     private void FixedUpdate()
     {
-        if (buttonPressed == RIGHT)
+
+
+
+        if (IsInputInList(RIGHT))
         {
-            rb2d.AddForce(new Vector2(moveSpeed, 0), ForceMode2D.Impulse);
+            if (!canJump)
+            {
+                
+                rb2d.AddForce(new Vector2(moveSpeed / 2, 0), ForceMode2D.Impulse);
+
+            }
+            else
+            {
+                rb2d.AddForce(new Vector2(moveSpeed, 0), ForceMode2D.Impulse);
+            }
         }
-        else if (buttonPressed == LEFT)
+        if (IsInputInList(LEFT))
         {
-            rb2d.AddForce(new Vector2(-moveSpeed, 0), ForceMode2D.Impulse);
+
+            if (!canJump)
+            {
+                rb2d.AddForce(new Vector2(-moveSpeed / 2, 0), ForceMode2D.Impulse);
+
+            }
+            else
+            {
+                rb2d.AddForce(new Vector2(-moveSpeed, 0), ForceMode2D.Impulse);
+
+            }
+        }
+        if (IsInputInList(JUMP))
+        {
+            CheckGroundStatus();
+            Debug.Log("aaaaaaaaaa");
+        }
+        inputList.Clear();
+    }
+    /// <summary>
+    /// Metodo que tira entre 1 a 3 raycast para comprobar si se esta tocando el suelo o no  el primero hacia abajo y los otros 2 en diagonal para assegurarse de poder saltar en bordes
+    /// </summary>
+    void CheckGroundStatus()
+    {
+
+        if (DrawRay(Vector2.down,Color.red) || DrawRay(Quaternion.Euler(-0.2f, 0, -45) * Vector3.down,Color.black) || DrawRay( new Vector3(0.2f,-1,0),Color.green))
+        {
+            rb2d.velocity = Vector2.zero;
+            rb2d.AddForce(new Vector2(0, moveSpeed * jumpForce - rb2d.velocity.x * this.acceleration), ForceMode2D.Impulse);
+
+            canJump = true;
+            Debug.Log(canJump);
         }
         else
         {
-        
+            canJump = false;
+            Debug.Log(canJump);
+        }
+
+
+    }
+    /// <summary>
+    /// Metodo que tira un raycast con una direction para ver si choca con algo 
+    /// </summary>
+    /// <param name="direction"> la direction hacia donde ira el rayo en vector2</param>
+    /// <param name="color"> el color del rayo</param>
+    /// <returns> un booleano para saber si ha collisionado o no</returns>
+    private bool DrawRay(Vector2 direction , Color color)
+    {
+        Debug.DrawRay(transform.position, Vector3.down, color);
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, direction, DeploymentHeight, groundMask);
+        // Debug.DrawRay(hit);
+        if (hit.collider == null)
+        {
+            return false;
+        }
+        else
+        {
+         
+            return true;
         }
     }
-    public void Move()
+    /// <summary>
+    /// Metodo para mirar si un input ha sido introducido
+    /// </summary>
+    /// <param name="input"> el nombre del input a mirar</param>
+    /// <returns> si esta o no </returns>
+    public bool IsInputInList(String input) {
+
+        return inputList.Contains(input);
+    }
+    /// <summary>
+    /// Metodo para añadir inputs a una arraylist  
+    /// </summary>
+    public void InputHandler()
     {
         if (Input.GetKey(KeyCode.RightArrow))
         {
-            buttonPressed = RIGHT;
+            inputList.Add(RIGHT);
 
 
         }
         else if (Input.GetKey(KeyCode.LeftArrow))
         {
-            buttonPressed = LEFT;
+            inputList.Add(LEFT);
+      
+
+
+        }
+        else if (Input.GetKey(jump))
+        {
+            inputList.Add(JUMP);
 
 
         }
